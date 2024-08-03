ced1fdd406fba7b615d2d9ed06eddbc83b9125ef
Andre <92643602+shouraki@users.noreply.github.com>
2024-08-03 15:32:40 +0100

    added hooks

--- day1.php
+++ day1.php
@@ -9,4 +9,25 @@ Author: Shouraki
 function myfirstplugin_active(){
     add_option ('my_firstplugin_install_date', current('mysql'));
 }
+register_activation_hook(__FILE__, 'myfirstplugin_activate');
+
+function myfirstplugin_deactive(){
+    delete_option ('my_firstplugin_install_date');
+}
+register_deactivation_hook(__FILE__, 'myfirstplugin_deactivate');
+
+function footer_content(){
+    echo "<p>This is footer content</p>";
+}
+add_action ('wp_footer', 'footer_content');
+
+function modify_post_title(){
+    if($title == 'Hello World!')
+{
+    return "welcome to my website";
+}
+return $title;
+}
+
+add_filter('the_title', 'modify_post_title');
 
